a1b055d49b7b336f9a0593c1e8b9f67fb7dad2b4
Zurc <cruzjurado@gmail.com>
2025-08-15 18:22:35 +0100

    back changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/elenora-green-coaching/",
   plugins: [react()],
+  base: "/elenora-green-coaching/",
+  build: {
+    outDir: "build",
+  },
   optimizeDeps: {
     exclude: ["lucide-react"],
   },
