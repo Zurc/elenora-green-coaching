cc120227df852c337cc5c83a5c36d06e6ba17c95
Zurc <cruzjurado@gmail.com>
2025-08-15 18:13:08 +0100

    index and vite config changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: "build",
-  },
   base: "/elenora-green-coaching/",
   plugins: [react()],
   optimizeDeps: {
