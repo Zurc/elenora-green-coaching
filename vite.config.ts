352de33cb257657812e2cf5bd774008cd7bc6ec2
Zurc <cruzjurado@gmail.com>
2025-08-15 04:20:51 +0100

    test

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/elenora-green-coaching/",
+  base: "/",
   publicDir: "public",
   build: {
     outDir: "dist",
